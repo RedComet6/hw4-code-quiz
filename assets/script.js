759beb9ddb0a488f9fe13a473e96035e566bd299
Aidan Chamberlain <asunshine99@gmail.com>
2022-01-07 20:52:17 -0600

    rearranged function setup, added "if" checks, added questions

--- assets/script.js
+++ assets/script.js
@@ -12,18 +12,48 @@ const questions = [
         answers: ["Periphery", "Northlane", "Spiritbox", "Meshuggah"],
         correct: "Meshuggah",
     },
+    {
+        title: "I have a beautiful little cat that I adopted in 2017, do you know her name?",
+        answers: ["Samus", "Artemis", "Olive", "Blossom"],
+        correct: "Blossom",
+    },
+    {
+        title: "I love tattoos! Ask me about them sometime, I will talk your ear off. After I get my next one over the summer, how many do you think that will make for me?",
+        answers: ["3", "6", "9", "12"],
+        correct: "6",
+    },
+    {
+        title: "Do you feel like you know me better now?",
+        answers: ["Yee!", "Nah..."],
+        correct: "Yee!",
+    },
 ];
 
 // functions
 function gameStart() {
-    questDiv.innerHTML = questions[0].title;
+    questionInit(0);
+}
 
-    questions[0].answers.forEach((answer) => {
+function questionInit(questionNum) {
+    // let correct = questions[questionNum].correct;
+    let removePrev = document.querySelectorAll(".answerOption");
+
+    if (removePrev.length > 0) {
+        removePrev.forEach((button) => {
+            button.style.display = "none";
+        });
+    }
+
+    questDiv.innerHTML = questions[questionNum].title;
+
+    questions[questionNum].answers.forEach((answer) => {
         // create element button for each possible answer
         const ansBtn = document.createElement("button");
 
         ansBtn.textContent = answer;
         ansBtn.setAttribute("value", answer);
+        ansBtn.setAttribute("class", "answerOption");
+        ansBtn.setAttribute("data-attr", questionNum);
         ansDiv.appendChild(ansBtn);
         ansBtn.onclick = ansClick;
     });
@@ -31,10 +61,18 @@ function gameStart() {
 
 // function for answer clicking
 function ansClick() {
-    alert("BLAM-O");
-    // check for correct
-    // timer
-    // display next
+    let chosen = this.value;
+    let questionNum = parseInt(this.getAttribute("data-attr"));
+
+    if (questions[questionNum].correct === chosen) {
+        console.log("made it");
+    }
+    console.log(chosen);
+    console.log(questionNum);
+    console.log(questions[questionNum].correct);
+    if (questionNum + 1 < questions.length) {
+        questionInit(questionNum + 1);
+    }
 }
 // function to end game
 
