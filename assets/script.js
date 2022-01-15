205bb620c9b0a52870319b30f9e28817e1c49df4
Aidan Chamberlain <asunshine99@gmail.com>
2022-01-14 23:02:45 -0600

    added bootstrap link, comments

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
+// assigns the html elements to const
 const quizTitle = document.querySelector("#quizTitle");
 const startBtn = document.querySelector("#start");
 const timerDiv = document.querySelector("#timer");
 const questDiv = document.querySelector("#question");
 const ansDiv = document.querySelector("#answers");
 const highScoreDiv = document.querySelector("#highScores");
+// array of questions for users to answer
 const questions = [
     {
         title: "I am a big fan of horror movies, do you happen to know my favorite horror film?",
@@ -31,34 +33,41 @@ const questions = [
         correct: "Coffee Mugs",
     },
 ];
+// inital value to kick off quiz at first question always
 const questionIndexInit = 0;
+// the maximum number of users who can be stored on the high score list
 const highScoreMaxIndex = 2;
+// pulls the high scores out of the local storage, parsed for use as an array of objects
 let highScores = JSON.parse(localStorage.getItem("highScoreList"));
+// inital scoring values
 let timerCount = 60;
 let gameOver = false;
 let correctChoices = 0;
 
 // functions
+// button activates this function to begin the quiz
 function gameStart() {
     timerStart();
     questionInit(questionIndexInit);
 }
 
+// function to rearrange the page elements according to which question is active
 function questionInit(questionNum) {
+    // removes the buttons from previous question to make way for new question's buttons
     let removePrev = document.querySelectorAll(".answerOption");
-
     if (removePrev.length > 0) {
         removePrev.forEach((button) => {
             button.style.display = "none";
         });
     }
 
+    // sets the current question's text for user to read
     questDiv.innerHTML = questions[questionNum].title;
-
+    // creates answers
     questions[questionNum].answers.forEach((answer) => {
-        // create element button for each possible answer
+        // creats button for each possible answer
         const ansBtn = document.createElement("button");
-
+        // displays text for each answer in button, applies attributes, appends to answer <div>, adds click event listener
         ansBtn.textContent = answer;
         ansBtn.setAttribute("value", answer);
         ansBtn.setAttribute("class", "answerOption");
@@ -68,13 +77,16 @@ function questionInit(questionNum) {
     });
 }
 
-// function for answer clicking
+// function to handle user clicking an answer button
 function ansClick() {
+    // assign variables to button's information
     let chosen = this.value;
     let questionNum = parseInt(this.getAttribute("data-attr"));
 
+    // checks if the correct answer was chosen
     if (questions[questionNum].correct === chosen) {
         alert("You got it right!");
+        // increment variable used in scoring
         correctChoices++;
     } else {
         alert("You got it wrong.");
@@ -82,33 +94,37 @@ function ansClick() {
         timerCount = timerCount - 3;
     }
 
+    // checks where the user is in the quiz, if questions are remaining, moves to next question
     if (questionNum + 1 < questions.length) {
         questionNum++;
         questionInit(questionNum);
     } else {
+        // if no questions remain, game is over and related endgame functions called
         gameOver === true;
         handleUserScore();
         checkPlayAgain();
     }
 }
 
-// function to save high score
+// function to handle high scores
 function handleUserScore() {
+    // scoring logic to determine how well user has done
     let userScore = timerCount * correctChoices;
+    // initialize user identity and result object
     let userIdent = "";
     let userResult = {};
 
+    // checks high score viability; adds high score if the high score board is not full OR if user score is higher than the lowest score on the board
     if (highScores.length < highScoreMaxIndex + 1 || userScore > highScores[highScoreMaxIndex].score) {
         userIdent = prompt("You got a High Score! Enter Your Initals");
+        // object to pass into storage
         userResult = {
             score: userScore,
             identity: userIdent,
         };
-
+        // adds new high score into high score array
         highScores.push(userResult);
-
-        console.log(highScores);
-
+        // sorts the high score array from highest score in index 0 to lowest score in index final
         highScores.sort(function (a, b) {
             let scoreA = a.score;
             let scoreB = b.score;
@@ -123,29 +139,36 @@ function handleUserScore() {
         });
     }
 
+    // if the newly added high score makes the array longer than the max length, remove the lowest score on the board after being sorted
     if (highScores.length > highScoreMaxIndex + 1) {
         highScores.pop();
     }
 
-    console.log(highScores);
+    // sends the high scores to local storage
     localStorage.setItem("highScoreList", JSON.stringify(highScores));
 }
 
+// function to handle user choosing to play again or not
 function checkPlayAgain() {
+    // ask user what they wish to do
     let playAgain = confirm("Would you like to play again?");
 
+    // if user wants to play again, reload the page to initial setup
     if (playAgain === true) {
         location.reload();
     } else {
+        // if user does not want to play again, clear all question related elements and display high score board
         startBtn.remove();
         timerDiv.remove();
         questDiv.remove();
         ansDiv.remove();
         quizTitle.textContent = "HIGH SCORES";
 
+        // creates a table element, assigns to variable, appends to high score <div>
         let scoreTable = document.createElement("table");
         highScoreDiv.appendChild(scoreTable);
 
+        // for each high score, add a row to the board that has an element for the score and an element for the identity
         highScores.forEach((obj) => {
             let scoreRow = document.createElement("tr");
             let scoreValue = document.createElement("td");
@@ -154,6 +177,7 @@ function checkPlayAgain() {
             scoreValue.textContent = obj.score;
             scoreIdentity.textContent = obj.identity;
 
+            // appends to the table in the high score <div>
             scoreTable.appendChild(scoreRow);
             scoreRow.appendChild(scoreValue);
             scoreRow.appendChild(scoreIdentity);
@@ -161,7 +185,7 @@ function checkPlayAgain() {
     }
 }
 
-// function for timer
+// function for keeping time
 function timerStart() {
     let timer = setInterval(function () {
         if (timerCount <= 1 || gameOver === true) {
@@ -173,5 +197,5 @@ function timerStart() {
     }, 1000);
 }
 
-//  page init
+//  page initial setup
 startBtn.addEventListener("click", gameStart);
